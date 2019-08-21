6ad558e519c94a1d61e1bc8bd265bed33ec69e71
axa25 <emevig@gmail.com>
2019-08-21 13:16:38 +0200

    formatting with prettier

--- errorImplementations/scripts/CustomErrorClass.mjs
+++ errorImplementations/scripts/CustomErrorClass.mjs
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 
 export class CustomErrorClass extends Error {
-    // firefox doesn't support fields
-    // code = "Default ExampleErrorCode";
-    // exampleProperty = "Default valueOfExampleProperty";
-    // customProperty = "Default valueOfCustomProperty";
+  // firefox doesn't support fields
+  // code = "Default ExampleErrorCode";
+  // exampleProperty = "Default valueOfExampleProperty";
+  // customProperty = "Default valueOfCustomProperty";
 
-    // override constructor
-    constructor( message, code, exampleProperty, customProperty ) {
-        // firefox doesn't treats only super( message ) constructor as Error class constructor
-        super( message );
-        // super();
-        super.message = message;
-        this.message = message;
-        super.name = "Error";
-        this.name = "CustomErrorClass";
-        this.name = this.name + " extends " + super.name;
+  // override constructor
+  constructor(message, code, exampleProperty, customProperty) {
+    // firefox doesn't treats only super( message ) constructor as Error class constructor
+    super(message);
+    // super();
+    super.message = message;
+    this.message = message;
+    super.name = "Error";
+    this.name = "CustomErrorClass";
+    this.name = this.name + " extends " + super.name;
 
-        this.code = code;
-        this.exampleProperty = exampleProperty;
-        this.customProperty = customProperty;
+    this.code = code;
+    this.exampleProperty = exampleProperty;
+    this.customProperty = customProperty;
 
-        // Error.captureStackTrace - to streamline stack trace // node.js
-        // make CustomErrorClass constructor( message ) not part of the resulting stuck trace
-        // Error.captureStackTrace(this, CustomErrorClass);
-    }
+    // Error.captureStackTrace - to streamline stack trace // node.js
+    // make CustomErrorClass constructor( message ) not part of the resulting stuck trace
+    // Error.captureStackTrace(this, CustomErrorClass);
+  }
 
-    setCode( code ) {
-        this.code = code;
-    }
+  setCode(code) {
+    this.code = code;
+  }
 
-    getCode() {
-        return this.code;
-    }
+  getCode() {
+    return this.code;
+  }
 }
 
 // export default CustomErrorClass;
--- functionsAndScriptFiles/scripts/secondFunctionsAndScriptFiles.js
+++ functionsAndScriptFiles/scripts/secondFunctionsAndScriptFiles.js
@@ -1,3 +1,4 @@
 function fourthFunction() {
-    document.getElementById('fourthParagraph').innerHTML = 'Fourth paragraph - function defined in seperate (second) script file .js';
+  document.getElementById("fourthParagraph").innerHTML =
+    "Fourth paragraph - function defined in seperate (second) script file .js";
 }
